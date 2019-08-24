951f98f2a257790a0f05c6054e7f0013e13547d0
gsx <gsx@kdanmobile.com>
2019-08-24 14:09:05 +0800

    cap - fix deploy branch to master

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,6 @@ set :repo_url, "git@github.com:g2ooola/job_post.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :branch, 'cap'
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/deployer/rails_application"
